75a6d238a7ac71c509e8dcfac4ae2bc32097ad1e
bratets_so <c.copokuh@gmail.com>
2019-10-12 18:23:23 +0300

    iShop v.1

---
+++ iShop/ShopBlock.js
@@ -0,0 +1,39 @@
+﻿var ShopBlock = React.createClass({
+
+  displayName: 'ShopBlock',
+
+  getDefaultProps: function () {
+    return { shopNameText: "Магазин 'Without Name'" }
+  },
+
+  render: function () {
+
+    var goodsCode = [];
+    this.props.goods.forEach(element => {
+      var goodCode =
+        React.DOM.tr({ key: element.itemNumber, className: 'Good' },
+          React.DOM.td({ className: 'GoodName' }, element.name),
+          React.DOM.td({ className: 'GoodCost' }, element.cost),
+          React.DOM.td({ className: 'GoodPhotoURL' }, 
+          React.DOM.a({href:element.photoURL},element.name)
+          ),
+          React.DOM.td({ className: 'GoodRest' }, element.rest),
+        )
+      goodsCode.push(goodCode);
+    });
+
+    return React.DOM.div({ className: 'ShopBlock' },
+      React.DOM.div({ className: 'ShopName' }, this.props.shopName),
+      React.DOM.table({ className: 'ShopTable' },
+        React.DOM.tr({ className: 'ShopTableHeader' },
+          React.DOM.td({}, 'Наименование товара'),
+          React.DOM.td({}, 'Стоимость товара'),
+          React.DOM.td({}, 'Ссылка на фото товара'),
+          React.DOM.td({}, 'Наличие на складе')
+        ),
+        ({}, goodsCode)
+      ),
+    )
+  }
+
+});
