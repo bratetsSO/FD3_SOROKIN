414a0d74f827efe85e93a5c0bdb50e04dfa8435e
bratets_so <c.copokuh@gmail.com>
2019-10-13 11:32:39 +0300

    add tbody

--- iShop/ShopBlock.js
+++ iShop/ShopBlock.js
@@ -14,8 +14,8 @@
         React.DOM.tr({ key: element.itemNumber, className: 'Good' },
           React.DOM.td({ className: 'GoodName' }, element.name),
           React.DOM.td({ className: 'GoodCost' }, element.cost),
-          React.DOM.td({ className: 'GoodPhotoURL' }, 
-          React.DOM.a({href:element.photoURL},element.name)
+          React.DOM.td({ className: 'GoodPhotoURL' },
+            React.DOM.a({ href: element.photoURL }, element.name)
           ),
           React.DOM.td({ className: 'GoodRest' }, element.rest),
         )
@@ -25,13 +25,15 @@
     return React.DOM.div({ className: 'ShopBlock' },
       React.DOM.div({ className: 'ShopName' }, this.props.shopName),
       React.DOM.table({ className: 'ShopTable' },
-        React.DOM.tr({ className: 'ShopTableHeader' },
-          React.DOM.td({}, 'Наименование товара'),
-          React.DOM.td({}, 'Стоимость товара'),
-          React.DOM.td({}, 'Ссылка на фото товара'),
-          React.DOM.td({}, 'Наличие на складе')
+        React.DOM.tbody({},
+          React.DOM.tr({ className: 'ShopTableHeader' },
+            React.DOM.td({}, 'Наименование товара'),
+            React.DOM.td({}, 'Стоимость товара'),
+            React.DOM.td({}, 'Ссылка на фото товара'),
+            React.DOM.td({}, 'Наличие на складе')
+          ),
+          ({}, goodsCode)
         ),
-        ({}, goodsCode)
       ),
     )
   }
